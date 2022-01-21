64818426e46b83d2314cffdcfdfe527109bf29b4
Ritu Sahu <70282987+rituu03@users.noreply.github.com>
2022-01-21 19:21:42 +0530

    Update PaginationButtonsList.js

--- src/components/PaginationButtonsList.js
+++ src/components/PaginationButtonsList.js
@@ -3,9 +3,30 @@ const PaginationButtonsList = () => {
 
     return (
         <div className="pagination-buttons-list">
-
+           <button id="button">1</button>
+           <button id="button">2</button>
+           <button id="button">3</button>
+           <button id="button">4</button>
+           <button id="button">5</button>
+           <button id="button">6</button>
+           <button id="button">7</button>
+           <button id="button">8</button>
+           <button id="button">9</button>
+           <button id="button">10</button>
+           <button id="button">11</button>
+           <button id="button">12</button>
+           <button id="button">13</button>
+           <button id="button">14</button>
+           <button id="button">15</button>
+           <button id="button">16</button>
+           <button id="button">17</button>
+           <button id="button">18</button>
+           <button id="button">19</button>
+           <button id="button">20</button>
         </div>
     )
 }
 
 export { PaginationButtonsList }
+
+
